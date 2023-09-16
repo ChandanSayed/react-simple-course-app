0bbabac0f16f93c6bdb7799a981cd479c6cc9c0d
Chandan Sayed <hangzhou635@gmail.com>
2023-09-16 23:10:22 +0600

    JSON folder location changed

--- src/components/CourseContainer.jsx
+++ src/components/CourseContainer.jsx
@@ -34,7 +34,7 @@ const CourseContainer = ({ setRemainingHour, setCreditHour, remainingHour, setCo
   }
 
   useEffect(() => {
-    fetch('./course-lists.json')
+    fetch('courses.json')
       .then(response => response.json())
       .then(data => setCourses(data));
   }, []);
